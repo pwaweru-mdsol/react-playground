3f94ecf6c4882a563760df2e73745cade700c95a
paulmbw <paul.waweru58@gmail.com>
2018-05-19 16:19:44 +0100

    wip

--- App.js
+++ App.js
@@ -6,17 +6,12 @@ import 'normalize.css/normalize.css';
 
 import AppRouter from './src/routers/AppRouter';
 import configureStore from './src/store/configureStore';
-import { addExpense } from './src/actions/expenses';
 import { setTextFilter } from './src/actions/filters';
 import "react-dates/lib/css/_datepicker.css";
 
 import { Provider } from 'react-redux';
 
 const store = configureStore();
-
-// store.dispatch(addExpense({ description: 'Water Bill', amount: 4500, createdAt: 1}));
-// store.dispatch(addExpense({ description: 'Gas Bill', amount: 1000, createdAt: 2}));
-// store.dispatch(addExpense({ description: 'Coffee', amount: 3243432, createdAt: 3}));
 
 const App = (
 	<Provider store={store}>
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,53 @@
 const path = require('path')
 const publicPath = path.resolve(__dirname, 'public');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-	entry: './App.js',
-	// entry: './playground/hoc.js',
-	mode: 'development',
-	output: {
-		path: publicPath,
-		filename: 'bundle.js'
-	},
-	module: {
-		rules: [
-			{
-				loader: 'babel-loader',
-				test: /\.js$/,
-				exclude: /node_modules/
-			},
-			{
-				test: /\.s?css$/,
-				use:[
-					'style-loader',
-					'css-loader',
-					'sass-loader'
-				]
-			}
-		]
-	},
-	devtool: 'cheap-module-eval-source-map',
-	devServer: {
-		contentBase: publicPath,
-		port: 9000,
-		historyApiFallback: true
+module.exports = env => {
+	const isProd = env === 'production';
+	const CSSExtract = new ExtractTextPlugin('styles.css');
+	return {
+		entry: './App.js',
+		mode: 'development',
+		output: {
+			path: publicPath,
+			filename: 'bundle.js'
+		},
+		module: {
+			rules: [
+				{
+					loader: 'babel-loader',
+					test: /\.js$/,
+					exclude: /node_modules/
+				},
+				{
+					test: /\.s?css$/,
+					use: CSSExtract.extract({
+						use:[
+							{
+								loader: 'css-loader',
+								options: {
+									sourceMap: true
+								}
+							},
+							{
+								loader: 'sass-loader',
+								options: {
+									sourceMap: true
+								}
+							}
+						]
+					})
+				}
+			]
+		},
+		plugins: [
+			CSSExtract
+		],
+		devtool: isProd ? 'source-map' : 'inline-source-map',
+		devServer: {
+			contentBase: publicPath,
+			port: 9000,
+			historyApiFallback: true
+		}
 	}
-};
-
+}	
